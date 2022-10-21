98e1adc685b892cfbd6d2f67e5003f2854af9ac9
Theresa Morcos <resis_strudel@gmx.de>
2022-10-21 16:39:25 -0700

    feat: implement search component

--- components/utility/search/Search.tsx
+++ components/utility/search/Search.tsx
@@ -1,11 +1,21 @@
+import {useState} from 'react';
 import styles from './Search.module.css';
 
 export interface ISearch {
-  sampleTextProp: string;
 }
 
-const Search: React.FC<ISearch> = ({ sampleTextProp }) => {
-  return <div className={styles.container}>{sampleTextProp}</div>;
+const Search: React.FC<ISearch> = () => {
+  const [searchTerm, setSearchTerm] = useState<string>();
+
+  return (
+    <form className="flex flex-col items-center gap-y-5" onSubmit={(e) => {e.preventDefault(); alert(searchTerm)}}>
+      <input type="text" className="rounded-full border-2 w-5/6 sm:w-96 h-12 px-3" value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)}/>
+      <div className="space-x-3">
+      <button type="submit" className="btn-primary">Google Search</button>
+      <button type="submit" className="btn-primary">I'm feeling lucky</button>
+      </div>
+    </form>
+  );
 };
 
 export default Search;
